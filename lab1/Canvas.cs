eb3a34a2eced98e739f28757ed032cb7e866f331
Aleksander Kuś <01151536@pw.edu.pl>
2021-10-13 19:44:30 +0200

    add functions to add a polygon

--- lab1/Canvas.cs
+++ lab1/Canvas.cs
@@ -25,6 +25,16 @@ namespace lab1
         }
 
         /// <summary>
+        /// Start adding a new polygon to the canvas
+        /// </summary>
+        /// <param name="location">Starting location of polygon</param>
+        public void StartAddingPolygon(Point mousePosition)
+        {
+            addingPolygonVertices = new();
+            addingPolygonVertices.Add(mousePosition);
+        }
+
+        /// <summary>
         /// Calculates distance between two points without the final root to avoid overhead
         /// </summary>
         /// <param name="p1"></param>
@@ -39,6 +49,35 @@ namespace lab1
         /// <param name="click_location">Mouse click location</param>
         /// <returns>If the point was clicked</returns>
         public bool IsPointClicked(Point point, Point click_location) => SquaredDistance(point, click_location) <= 100;
+
+        /// <summary>
+        /// Add a new point to the polygon
+        /// </summary>
+        /// <param name="location">The location</param>
+        /// <returns>If drawing of the new polygon is finished, return true</returns>
+        public bool AddPointToPolygon(Point mousePosition)
+        {
+            // if the first point was clicked, finish adding the new polygon
+            if (addingPolygonVertices.Count > 3 && IsPointClicked(addingPolygonVertices[0], mousePosition))
+            {
+                addingPolygonVertices.RemoveAt(addingPolygonVertices.Count - 1);
+                polygons.Add(new Shapes.Polygon(addingPolygonVertices));
+                Redraw();
+                return true;
+            }
+            // else add a new point to the polygon
+            else
+                addingPolygonVertices.Add(mousePosition);
+            Redraw();
+            return false;
+        }
+
+        
+        public void MouseMoveWhileAddingPolygon(Point mousePosition)
+        {
+            addingPolygonVertices[^1] = mousePosition;
+            Redraw();
+        }
 
     }
 }
