ebbb318ab39bcf210e5ac9a4a593e591f03a4710
Aleksander Kuś <01151536@pw.edu.pl>
2021-10-13 19:40:40 +0200

    add private canvas members to store elements and add new ones

--- lab1/Canvas.cs
+++ lab1/Canvas.cs
@@ -6,6 +6,23 @@ namespace lab1
 {
     public class Canvas
     {
- 
+        private BufferedPanel panel;
+
+        // storing elements on the canvas
+        private List<Shapes.Polygon> polygons = new();
+        private List<Shapes.Circle> circles = new();
+
+        // used when adding a new polygon
+        private List<Point> addingPolygonVertices;
+
+        // used when adding a new circle
+        private Shapes.Circle addingCircle;
+        private bool circle_anchored = false;
+
+        public Canvas(BufferedPanel panel)
+        {
+            this.panel = panel;
+        }
+
     }
 }
