96aa9cce3859eab7b336c417acf5150a5bd41218
Aleksander Kuś <01151536@pw.edu.pl>
2021-10-14 16:48:09 +0200

    automate the process of calculating polygon center point with observablecollection

--- lab1/Shapes/Polygon.cs
+++ lab1/Shapes/Polygon.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Drawing;
 using System.Linq;
 
@@ -6,13 +7,24 @@ namespace lab1.Shapes
 {
     public class Polygon
     {
-        public List<Point> VertexList { get; private set; }
-        public Point MiddlePoint { get; private set; }
+        public ObservableCollection<Point> VertexList { get; private set; }
+        public Point Center { get; private set; }
 
         public Polygon(List<Point> vertices)
         {
-            VertexList = vertices;
-            MiddlePoint = new Point(VertexList.Select(p => p.X).Aggregate((p1, p2) => p1 + p2) / VertexList.Count,
+            VertexList = new(vertices);
+            VertexList.CollectionChanged += VertexList_CollectionChanged;
+            VertexList_CollectionChanged(null, null);  // artifically call the method to draw the middle point on creation
+        }
+
+        /// <summary>
+        /// A method to automatically update center point position when VertexList changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void VertexList_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Center = new Point(VertexList.Select(p => p.X).Aggregate((p1, p2) => p1 + p2) / VertexList.Count,
                 VertexList.Select(p => p.Y).Aggregate((p1, p2) => p1 + p2) / VertexList.Count);
         }
     }
