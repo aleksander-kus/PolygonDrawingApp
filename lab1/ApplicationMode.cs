d707e84d92b96382cf0c13b05b329d6890579827
Aleksander Kuś <01151536@pw.edu.pl>
2021-10-14 15:42:30 +0200

    added new app modes for moving elements

--- lab1/ApplicationMode.cs
+++ lab1/ApplicationMode.cs
@@ -4,6 +4,10 @@
     {
         Default = 0,
         AddingPolygon = 1,
-        AddingCircle = 2
+        AddingCircle = 2,
+        MovingVertex = 3,
+        MovingPolygonCenter = 4,
+        MovingCircleCenter = 5,
+        ResizingCircle
     }
 }
