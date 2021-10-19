6f5253f2ed3280eb83812e08a68df76365058815
Aleksander Kuś <01151536@pw.edu.pl>
2021-10-19 11:13:28 +0200

    implement bresenhams line algorithm

--- lab1/Helpers/GraphicsHelpers.cs
+++ lab1/Helpers/GraphicsHelpers.cs
@@ -41,5 +41,70 @@ namespace lab1
         /// <param name="p2"></param>
         /// <returns>Distance between two points rounded down</returns>
         public static int Distance(Point p1, Point p2) => (int)Math.Sqrt(SquaredDistance(p1, p2));
+
+        public static void PutPixel(this Graphics g, Brush b, int x, int y) => g.FillRectangle(b, x, y, 1, 1);
+
+        /// <summary>
+        /// Draws a between specified points with Bresenham's Algorithm
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="pen"></param>
+        /// <param name="p1">Starting point</param>
+        /// <param name="p2">Ending point</param>
+        public static void DrawLineBresenham(this Graphics g, Pen pen, Point p1, Point p2)
+        {
+            Brush b = new SolidBrush(pen.Color);
+            int dx = Math.Abs(p2.X - p1.X), dy = Math.Abs(p2.Y - p1.Y);
+            int x_increment = (p1.X < p2.X) ? 1 : p1.X == p2.X ? 0 : -1;
+            int y_increment = (p1.Y < p2.Y) ? 1 : p1.Y == p2.Y ? 0 : -1;
+            // first pixel
+            int x = p1.X, y = p1.Y;
+            g.PutPixel(b, x, y);
+            // go along X-axis
+            if (dx > dy)
+            {
+                int d = 2 * dy - dx;
+                int across_increment = (dy - dx) * 2;
+                int same_line_increment = 2 * dy;
+                // pętla po kolejnych x
+                while (x != p2.X)
+                {
+                    if (d >= 0)  // go across
+                    {
+                        x += x_increment;
+                        y += y_increment;
+                        d += across_increment;
+                    }
+                    else  // remain in the same line
+                    {
+                        x += x_increment;
+                        d += same_line_increment;
+                    }
+                    g.PutPixel(b, x, y);
+                }
+            }
+            //  go along Y-axis
+            else
+            {
+                int d = 2 * dx  - dy;
+                int across_increment = (dx - dy) * 2;
+                int same_line_increment = 2 * dx;
+                while (y != p2.Y)
+                {
+                    if (d >= 0)  // go across
+                    {
+                        x += x_increment;
+                        y += y_increment;
+                        d += across_increment;
+                    }
+                    else  // remain in the same line
+                    {
+                        y += y_increment;
+                        d += same_line_increment;
+                    }
+                    g.PutPixel(b, x, y);
+                }
+            }
+        }
     }
 }
