d11ea86cb50d7584fff785ff35e6f666d016f70e
Aleksander Kuś <01151536@pw.edu.pl>
2021-10-20 20:44:19 +0200

    added a class to represent an edge

---
+++ lab1/Shapes/Edge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1.Shapes
+{
+    public class Edge
+    {
+        public Point p1 { get; set; }
+        public Point p2 { get; set; }
+
+        public Edge(Point p1, Point p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+    }
+}
