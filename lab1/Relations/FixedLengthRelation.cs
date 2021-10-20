1868491e2da852fec6612c193abaf9641cf28acb
Aleksander Kuś <01151536@pw.edu.pl>
2021-10-20 22:35:31 +0200

    clean up equal length relation class

--- lab1/Relations/FixedLengthRelation.cs
+++ lab1/Relations/FixedLengthRelation.cs
@@ -20,20 +20,6 @@ namespace lab1.Relations
         public override void MovePoint(Shapes.Point clickedPoint, int delta_x, int delta_y)
         {
             Shapes.Point other = edge.p1 == clickedPoint ? edge.p2 : edge.p1;
-            //clickedPoint.X += delta_x;
-            //clickedPoint.Y += delta_y;
-
-            //// the other point has only one relation (this one), no further actions required
-            //if (other.R1 == null || other.R2 == null)
-            //{
-            //    other.X = other.X + delta_x;
-            //    other.Y = other.Y + delta_y;
-            //    return;
-            //}
-            //Relation otherRelation = other.R1 == this ? other.R2 : other.R1;
-            //otherRelation.MovePoint(clickedPoint, new Shapes.Point(clickedPoint.X + delta_x, clickedPoint.Y + delta_y));
-            //other.X += delta_x;
-            //other.Y += delta_y;
             other.Move(other.X + delta_x, other.Y + delta_y, clickedPoint);
         }
     }
